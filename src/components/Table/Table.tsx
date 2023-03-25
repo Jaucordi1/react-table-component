3e9470749fa87de5497f2fa3ac6fb7914bb54f17
Jason Aucordier <contact@jaucordi.fr>
2023-03-25 21:35:42 +0100

    Adding CSS
    - Rollup postcss plugin
    - Table.component.css file
    - classnames library

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,3 +1,5 @@
+import './Table.component.css';
+import classNames from 'classnames';
 import React from 'react';
 
 function valueToString(value: unknown): string {
@@ -50,13 +52,13 @@ function extractColumns<T extends { [key: string]: unknown }, K extends keyof T>
     );
 }
 
-export interface TableProps<T extends {}, K extends keyof T> {
+export interface TableProps<T extends {}, K extends keyof T> extends React.TableHTMLAttributes<HTMLTableElement> {
     lines: T[];
     columns?: K[];
 }
 
 export default function Table<T extends {}, K extends keyof T>(props: TableProps<T, K>) {
-    let {lines, columns, ...mainProps} = props;
+    let {lines, columns, className, ...mainProps} = props;
     if (!columns) {
         columns = Array.from(
             lines.reduce((keys, line) => {
@@ -67,21 +69,23 @@ export default function Table<T extends {}, K extends keyof T>(props: TableProps
     }
 
     return (
-        <table {...mainProps}>
+        <table {...mainProps} className={classNames('table', className)}>
             {columns && (
-                <thead>
-                <tr>
+                <thead className="table-head">
+                <tr className="table-row">
                     {columns.map((column, idx) => (
-                        <th key={idx}>{String(column)}</th>
+                        <th key={idx} className="table-head-cell">
+                            {String(column)}
+                        </th>
                     ))}
                 </tr>
                 </thead>
             )}
-            <tbody>
+            <tbody className="table-body">
             {lines.map((line, lineIdx) => (
-                <tr key={lineIdx}>
+                <tr key={lineIdx} className="table-row">
                     {columns!.map((column, columnIdx) => (
-                        <td key={columnIdx}>
+                        <td key={columnIdx} className="table-data-cell">
                             {valueToString(getDotNotationValue(line, column))}
                         </td>
                     ))}
